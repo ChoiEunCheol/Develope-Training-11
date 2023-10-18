4726d85c5c866c33743ad2e9e99f1ecb1a81add1
최은철 <wndqhrgkdlel@gmail.com>
2023-10-18 14:51:35 +0900

    POST로 받기

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const http = require('http');
-const fs = require('fs');
-const querysrting = require('querystring');
+const http = require("http");
+const fs = require("fs");
+const querysrting = require("querystring");
 
-http.createServer((req,res)=>{
-    if (req.method === 'GET' && req.url === '/'){
-        fs.readFile('./Develope-Traning-11.html',(err,data)=>{
-            if(err){
-                console.log('html파일을 찾을 수 없습니다.');
-            }else{
-                res.writeHead(200, {'Content-Type':'text/html'});
-                res.end(data);
-        }})}
-}).listen(8080,()=>
-    console.log(`http://localhost:8080`));
+http
+  .createServer((req, res) => {
+    if (req.method === "GET" && req.url === "/") {
+      fs.readFile("./Develope-Training-11.html", (err, data) => {
+        if (err) {
+          console.log("html파일을 찾을 수 없습니다.");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        }
+      });
+    }
+    else if (req.method === "POST" && req.url === "/login") {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.end("ㅇㅇ")};
+    }).listen(8080, () => console.log(`http://localhost:8080`));

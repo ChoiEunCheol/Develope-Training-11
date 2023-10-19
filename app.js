bc4542366166b2137564a581ab2cf2351b966755
최은철 <wndqhrgkdlel@gmail.com>
2023-10-19 10:49:11 +0900

    기본 형태로 다시 돌림

--- app.js
+++ app.js
@@ -15,21 +15,15 @@ http
         }
       });
     } else if (req.method === "POST" && req.url === "/login") {
-      //   req.on('data', function(chunk){
-      //     console.log('chunk :',chunk);
-      //     console.log('parse(chunk) :', querysrting.parse(chunk));
-      //     console.log(chunk.toString());
-      //     var data = querysrting.parse(chunk.toString());
-      //     res.writeHead(200, {'Content-Type' : 'text/html'});
-      //     res.end(data.id + data.pw);
-      //     console.log('data :', data);
-      // });
-      // // res.writeHead(200, {'Content-Type' : 'text/html'});
-      // // res.end('ID : ' + data.id + 'PW : ' + data.pw);
-      var data = querysrting.parse(chunk.toString());
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data.id + data.pw);
-      console.log("data :", data);
+        req.on('data', function(chunk){
+          console.log('chunk :',chunk);
+          console.log('parse(chunk) :', querysrting.parse(chunk));
+          console.log(chunk.toString());
+          var data = querysrting.parse(chunk.toString());
+          res.writeHead(200, {'Content-Type' : 'text/html'});
+          res.end(data.id + data.pw);
+          console.log('data :', data);
+      });
     }
   })
   .listen(8080, () => console.log(`http://localhost:8080`));

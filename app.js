a9732e19a60705597a7b5e6738a7f4d204e895d6
최은철 <wndqhrgkdlel@gmail.com>
2023-10-19 12:35:26 +0900

    signUpAsset에 쓴 내용이 불러지지 않음

--- app.js
+++ app.js
@@ -16,29 +16,32 @@ http
         }
       });
     } else if (req.method === "POST" && req.url === "/login") {
-        req.on('data', function(chunk){
-          console.log('chunk :',chunk);
-          console.log('chunk.toString() :',chunk.toString());
-          console.log('parse(chunk) :', querysrting.parse(chunk));
-          console.log('parse(chunk.toString()) :', querysrting.parse(chunk.toString()));
-        
-          let data = querysrting.parse(chunk.toString());
+      req.on("data", function (chunk) {
+        console.log("chunk :", chunk);
+        console.log("chunk.toString() :", chunk.toString());
+        console.log("parse(chunk) :", querysrting.parse(chunk));
+        console.log(
+          "parse(chunk.toString()) :",
+          querysrting.parse(chunk.toString())
+        );
 
-          fs.writeFile("signUpAsset.js",`const signUpAsset = { id: '${data.id}', pw: '${data.pw}', repw: '${data.repw}', email: '${data.email}' }`,(err)=>{
-            if(err){
+        let data = querysrting.parse(chunk.toString());
+
+        fs.writeFile(
+          "signUpAsset.js",
+          `const signUpAsset = { id: '${data.id}', pw: '${data.pw}', repw: '${data.repw}', email: '${data.email}' }`,
+          (err) => {
+            if (err) {
               console.err("Error");
             } else {
               console.log("signUpAsset.js파일 생성");
+              res.writeHead(200, { "Content-Type": "text/html" });
+              res.end(
+                "id : " + signUpAsset.id + "pw : " + signUpAsset.password
+              );
             }
-          });
-
-          signUpAsset.id = data.id ; 
-          signUpAsset.password = data.pw ; 
-          signUpAsset.email = data.email ; 
-
-          res.writeHead(200, {'Content-Type' : 'text/html'});
-          res.end('id : '+ signUpAsset.id + 'pw : ' + signUpAsset.password);
-          console.log('data :', data);
+          }
+        );
       });
     }
   })

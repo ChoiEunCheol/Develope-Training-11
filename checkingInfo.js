e9611aed115413e4c8a0463e7214425ca500e7a9
최은철 <wndqhrgkdlel@gmail.com>
2023-10-20 10:25:32 +0900

    유효성 검사 끝 / 4개를 다 만족할 때 활성화 되도록 바꿔야 됨

--- checkingInfo.js
+++ checkingInfo.js
@@ -6,6 +6,7 @@ function submitdisabled(x) {
   submit.disabled = x;
 }
 
+// 유효성 검사
 inputTag[0].onchange = () => {
   switch (true) {
     case inputTag[0].value === inputTag[0].value.toUpperCase():
@@ -21,12 +22,29 @@ inputTag[0].onchange = () => {
       break;
   }
 };
-
 inputTag[1].onchange = () => {
   const compare = inputTag[1].value === inputTag[2].value;
   if (compare === true) {
     submitdisabled(false);
-  } else{
+  } else {
+    submitdisabled(true);
+  }
+};
+inputTag[2].onchange = () => {
+  const compare = inputTag[1].value === inputTag[2].value;
+  if (compare === true) {
+    submitdisabled(false);
+  } else {
+    submitdisabled(true);
+  }
+};
+inputTag[3].onchange = () => {
+  let atsign = inputTag[3].value.search("@");
+  let dot = inputTag[3].value.search('.com');
+  
+  if (dot > 0 && atsign > 0 && dot > atsign){
+    submitdisabled(false);
+  } else {
     submitdisabled(true);
   }
 };
